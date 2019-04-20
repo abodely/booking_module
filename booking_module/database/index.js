e77e855bae62bec653df0b41d596bbc467b93990
William Wong <williamwj.wong@gmail.com>
2019-04-19 20:14:43 -0700

    fixed spacing and took out comments

--- booking_module/database/index.js
+++ booking_module/database/index.js
@@ -3,13 +3,12 @@ const pw = process.env.MYSQL_ROOT_PW || require('./config/example.config').rootP
 
 const databaseName = process.env.MYSQL_DATABASE || 'bookings';
 
-
 const db = new Sequelize(databaseName, 'williamwong', '', {
   host: process.env.MYSQL_URL || 'localhost',
   dialect: 'postgres',
   logging: false,
 });
 
-// db.query('CREATE DATABASE IF NOT EXISTS bookings').then(() => console.log('Database created'));
+
 
 module.exports = db;
--- booking_module/database/seedCSV.js
+++ booking_module/database/seedCSV.js
@@ -1,7 +1,5 @@
 const db = require ('./index');
 
-
-
 db.query(`COPY reservations FROM '/Users/williamwong/Downloads/coding/SDC/booking_module/database/reservation2.csv' DELIMITER ',' CSV HEADER`)
   .then(console.log('success'))
   .catch(err => console.log(err))
